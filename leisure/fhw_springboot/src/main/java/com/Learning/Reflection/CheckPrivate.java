f20596be2c419d23426e1a7133e6a4002674acb2
fanhaowen <fanhaowen@meicai.cn>
2023-01-06 12:44:21 +0800

    ;

---
+++ leisure/fhw_springboot/src/main/java/com/Learning/Reflection/CheckPrivate.java
@@ -0,0 +1,5 @@
+package com.Learning.Reflection;
+
+public class CheckPrivate {
+
+}
--- leisure/fhw_springboot/src/main/java/com/Learning/Reflection/UnePrivateClass.java
+++ leisure/fhw_springboot/src/main/java/com/Learning/Reflection/UnePrivateClass.java
@@ -1,4 +1,24 @@
 package com.Learning.Reflection;
 
+import java.math.BigDecimal;
+
 public class UnePrivateClass {
+    public BigDecimal amount = new BigDecimal(100);
+
+    private BigDecimal addAmountPrivate(BigDecimal bigDecimal) {
+        if (bigDecimal.compareTo(BigDecimal.ZERO) < 0) {
+            return null;
+        }
+        this.amount = this.amount.add(bigDecimal);
+        return new BigDecimal(this.amount.toString());
+    }
+
+    public BigDecimal addAmountPublic(BigDecimal bigDecimal) {
+        if (bigDecimal.compareTo(BigDecimal.ZERO) < 0) {
+            return null;
+        }
+        this.amount = this.amount.add(bigDecimal);
+        return new BigDecimal(this.amount.toString());
+    }
+
 }
